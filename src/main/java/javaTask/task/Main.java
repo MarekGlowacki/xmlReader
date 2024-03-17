bc819ec6a32a176aa3d83a6ffcc08cff94e0ef93
User <user@its.me>
2024-03-17 09:50:45 +0200

    przykładowe wywołanie operacji w mainie

--- src/main/java/javaTask/task/Main.java
+++ src/main/java/javaTask/task/Main.java
@@ -2,6 +2,12 @@ package javaTask.task;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        EmployeeDatabase database = new EmployeeDatabase();
+        Person newEmployee = new Person("2", "Lukasz", "Doe", "123456789", "john.doe@example.com", "12345678901");
+
+        database.create(newEmployee);
+        database.find(EmployeeDatabase.Type.EXTERNAL, "Lukasz", "Doe", "123456789");
+        database.modify(newEmployee);
+        database.remove("1");
     }
 }
